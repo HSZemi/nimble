0f220fe51fe5d970db88dc7f3c9210c3cff0aefd
HSZemi <HSZemi@users.noreply.github.com>
2019-03-19 16:47:56 +0100

    Publish react version in docs folder

---
+++ docs/precache-manifest.b5abda30eaa458b3254e388cfd983cb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3639cfb56e91e65223c0",
+    "url": "/static/js/main.3496fcd1.chunk.js"
+  },
+  {
+    "revision": "a630980b3aee4c2b4c11",
+    "url": "/static/js/2.753d7e94.chunk.js"
+  },
+  {
+    "revision": "3639cfb56e91e65223c0",
+    "url": "/static/css/main.9a538e4d.chunk.css"
+  },
+  {
+    "revision": "336a26cf8349e30135b49e8d78bd048d",
+    "url": "/index.html"
+  }
+];
